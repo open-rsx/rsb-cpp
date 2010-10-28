22d8bbc8a94d2bdfece1c8f3d52a4bb797e541eb
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2010-10-28 09:36:58 +0000

    converter for boost::uint64_t

---
+++ src/rsb/transport/converter/Uint64Converter.cpp
@@ -0,0 +1,77 @@
+/* ============================================================
+ *
+ * This file is a part of the RSB project
+ *
+ * Copyright (C) 2010 by Johannes Wienke <jwienke at techfak dot uni-bielefeld dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "Uint64Converter.h"
+
+#include <rsc/misc/Registry.h>
+
+using namespace std;
+
+namespace rsb {
+
+namespace transport {
+
+Uint64Converter::Uint64Converter() {
+}
+
+Uint64Converter::~Uint64Converter() {
+}
+
+string Uint64Converter::getTypeName() {
+	return "uint64";
+}
+
+void Uint64Converter::serialize(const std::string &type,
+		boost::shared_ptr<void> data, string &m) {
+	// TODO common check for all converters, refactor to base class
+	if (type == getTypeName()) {
+		boost::shared_ptr<boost::uint64_t> number = boost::static_pointer_cast<
+				boost::uint64_t>(data);
+		stringstream s;
+		s << *number;
+		m = s.str();
+	}
+}
+
+boost::shared_ptr<void> Uint64Converter::deserialize(const std::string &type,
+		const string &d) {
+	boost::shared_ptr<void> p;
+	// TODO this check will be a common case. Refactor it to the base class
+	if (type == getTypeName()) {
+		stringstream s;
+		s << d;
+		boost::uint64_t number;
+		// TODO check fail bit
+		s >> number;
+		return boost::shared_ptr<uint64_t>(new boost::uint64_t(number));
+	} else {
+		// TODO better exception required
+		throw("No such type registered at TypeFactory!");
+	}
+	return p;
+}
+
+// we need this typedef for the macro to work
+typedef AbstractConverter<std::string> AbstractStringConverter;
+CREATE_GLOBAL_REGISTREE(AbstractStringConverter, Uint64Converter)
+;
+
+}
+
+}
---
+++ src/rsb/transport/converter/Uint64Converter.h
@@ -0,0 +1,52 @@
+/* ============================================================
+ *
+ * This file is a part of the RSB project
+ *
+ * Copyright (C) 2010 by Johannes Wienke <jwienke at techfak dot uni-bielefeld dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef UINT64CONVERTER_H_
+#define UINT64CONVERTER_H_
+
+#include "../AbstractConverter.h"
+
+#include <string>
+#include <boost/shared_ptr.hpp>
+
+namespace rsb {
+namespace transport {
+
+/**
+ * Converter for uint64 types.
+ *
+ * @author jwienke
+ */
+class Uint64Converter: public rsb::transport::AbstractConverter<std::string> {
+public:
+	Uint64Converter();
+	virtual ~Uint64Converter();
+
+	std::string getTypeName();
+
+	void serialize(const std::string &type, boost::shared_ptr<void> data, std::string &m);
+	boost::shared_ptr<void> deserialize(const std::string &type, const std::string &d);
+};
+
+typedef boost::shared_ptr<Uint64Converter> Uint64ConverterPtr;
+
+}
+}
+
+#endif /* UINT64CONVERTER_H_ */
