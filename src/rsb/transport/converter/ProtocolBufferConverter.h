9a3def8c5ca6c14e38dbef4181dba21f09bf354d
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-04-10 14:13:08 +0000

    Fix code style

--- src/rsb/transport/converter/ProtocolBufferConverter.h
+++ src/rsb/transport/converter/ProtocolBufferConverter.h
@@ -20,6 +20,7 @@
 #pragma once
 
 #include <string>
+
 #include <boost/shared_ptr.hpp>
 
 #include <rsc/runtime/TypeStringTools.h>
@@ -31,82 +32,79 @@ namespace transport {
 
 namespace detail {
 
-std::string
-type_name_to_proto_name(const std::string& type_name) {
-  std::string result = ".";
-  bool colon = false;
-  for (std::string::const_iterator it
-	 = type_name.begin(); it != type_name.end(); ++it) {
-    if (*it == ':') {
-      if (colon) {
-	colon = false;
-      } else {
-	result.push_back('.');
-	colon = true;
-      }
-    } else {
-      result.push_back(*it);
-    }
-  }
-  return result;
+std::string typeNameToProtoName(const std::string& type_name) {
+	std::string result = ".";
+	bool colon = false;
+	for (std::string::const_iterator it = type_name.begin(); it
+			!= type_name.end(); ++it) {
+		if (*it == ':') {
+			if (colon) {
+				colon = false;
+			} else {
+				result.push_back('.');
+				colon = true;
+			}
+		} else {
+			result.push_back(*it);
+		}
+	}
+	return result;
 }
 
-std::string
-type_name_to_data_type_name(const std::string& type_name) {
-  return "protocol-buffer:" + type_name_to_proto_name(type_name);
+std::string typeNameToDataTypeName(const std::string& type_name) {
+	return "protocol-buffer:" + typeNameToProtoName(type_name);
 }
 
 }
 
-template <typename ProtocolBuffer>
-class ProtocolBufferConverter : public rsb::transport::AbstractConverter<std::string> {
+template<typename ProtocolBuffer>
+class ProtocolBufferConverter: public rsb::transport::AbstractConverter<
+		std::string> {
 public:
-  ProtocolBufferConverter();
-  virtual
-  ~ProtocolBufferConverter();
+	ProtocolBufferConverter();
+	virtual
+	~ProtocolBufferConverter();
 
-  std::string
-  serialize(const AnnotatedData &data,
-	    std::string         &wire);
+	std::string
+	serialize(const AnnotatedData &data, std::string &wire);
 
-  AnnotatedData
-  deserialize(const std::string &wireType,
-	      const std::string &wire);
+	AnnotatedData
+	deserialize(const std::string &wireType, const std::string &wire);
 };
 
 // Implementation
 
-template <typename ProtocolBuffer>
+template<typename ProtocolBuffer>
 ProtocolBufferConverter<ProtocolBuffer>::ProtocolBufferConverter() :
-  AbstractConverter<std::string> (rsc::runtime::typeName<ProtocolBuffer>(),
-				  detail::type_name_to_data_type_name(rsc::runtime::typeName<ProtocolBuffer>())) {
+			AbstractConverter<std::string> (
+					rsc::runtime::typeName<ProtocolBuffer>(),
+					detail::typeNameToDataTypeName(
+							rsc::runtime::typeName<ProtocolBuffer>())) {
 }
 
-template <typename ProtocolBuffer>
+template<typename ProtocolBuffer>
 ProtocolBufferConverter<ProtocolBuffer>::~ProtocolBufferConverter() {
 }
 
-template <typename ProtocolBuffer>
-std::string
-ProtocolBufferConverter<ProtocolBuffer>::serialize(const AnnotatedData &data,
-						   std::string         &wireData) {
-  assert(data.first == getDataType());
+template<typename ProtocolBuffer>
+std::string ProtocolBufferConverter<ProtocolBuffer>::serialize(
+		const AnnotatedData &data, std::string &wireData) {
+	assert(data.first == getDataType());
 
-  boost::shared_ptr<ProtocolBuffer> s
-    = boost::static_pointer_cast<ProtocolBuffer>(data.second);
-  s->SerializeToString(&wireData);
-  return getWireSchema();
+	boost::shared_ptr<ProtocolBuffer> s = boost::static_pointer_cast<
+			ProtocolBuffer>(data.second);
+	s->SerializeToString(&wireData);
+	return getWireSchema();
 }
 
-template <typename ProtocolBuffer>
-AnnotatedData
-ProtocolBufferConverter<ProtocolBuffer>::deserialize(const std::string &wireSchema,
-						     const std::string &wireData) {
-  assert(wireSchema == getWireSchema());
+template<typename ProtocolBuffer>
+AnnotatedData ProtocolBufferConverter<ProtocolBuffer>::deserialize(
+		const std::string &wireSchema, const std::string &wireData) {
+	assert(wireSchema == getWireSchema());
 
-  boost::shared_ptr<ProtocolBuffer> result(new ProtocolBuffer());
-  result->ParseFromString(wireData);
-  return std::make_pair(getDataType(), result);
+	boost::shared_ptr<ProtocolBuffer> result(new ProtocolBuffer());
+	result->ParseFromString(wireData);
+	return std::make_pair(getDataType(), result);
 }
 
 }
