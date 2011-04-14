1fdde02d12f8584f76ef1b9faf6f722d39cc92af
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-04-14 18:33:32 +0000

    try to resolve all this funky stuff

---
+++ src/rsb/eventprocessing/Router.cpp
@@ -0,0 +1,119 @@
+/* ============================================================
+ *
+ * This file is a part of the RSB project
+ *
+ * Copyright (C) 2010 by Sebastian Wrede <swrede at techfak dot uni-bielefeld dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "Router.h"
+
+#include <rsc/logging/Logger.h>
+
+using namespace std;
+using namespace rsc::logging;
+using namespace rsb;
+using namespace rsb::filter;
+using namespace rsb::transport;
+
+namespace rsb {
+namespace eventprocessing {
+
+Router::Router(Factory::ConnectorTypes inType,
+		Factory::ConnectorTypes outType) :
+	logger(Logger::getLogger("rsb.transport.Router")) {
+	inConnector = Factory::createConnector(inType);
+	outConnector = Factory::createConnector(outType);
+	if (inConnector) {
+		eventProcessingStrategy = EventProcessingStrategyPtr(new EventProcessingStrategy());
+		// add event processor as observer to input port(s)
+		inConnector->setObserver(
+				boost::bind(&EventProcessingStrategy::process, eventProcessingStrategy, _1));
+	}
+	shutdown = false;
+}
+
+void Router::activate() {
+	if (inConnector) {
+		inConnector->activate();
+	}
+	if (outConnector) {
+		outConnector->activate();
+	}
+}
+
+void Router::deactivate() {
+	shutdown = true;
+	// TODO
+}
+
+Router::~Router() {
+	if (!shutdown) {
+		deactivate();
+	}
+}
+
+void Router::publish(EventPtr e) {
+	RSCDEBUG(logger, "Router::publish(Event) publishing new event: " << e);
+	outConnector->push(e);
+}
+
+void Router::notifyConnectors(rsb::SubscriptionPtr s,
+		rsb::filter::FilterAction::Types a) {
+	// TODO missing check if there really is an inport
+	FilterObserverPtr fo = boost::static_pointer_cast<FilterObserver>(inConnector);
+	for (rsb::FilterChain::iterator listIt = s->getFilters()->begin(); listIt
+			!= s->getFilters()->end(); ++listIt) {
+		// TODO check whether we want to do this also for out ports
+		// TODO generally use rsb::filters::Observable implementation here!
+		(*listIt)->notifyObserver(fo, a);
+	}
+}
+
+void Router::subscribe(rsb::SubscriptionPtr s) {
+	// notify ports about new subscription
+	notifyConnectors(s, rsb::filter::FilterAction::ADD);
+	// TODO missing check if there really is an inport and ep
+	eventProcessingStrategy->subscribe(s);
+}
+
+void Router::unsubscribe(rsb::SubscriptionPtr s) {
+	// notify ports about removal of subscription
+	notifyConnectors(s, rsb::filter::FilterAction::REMOVE);
+	// TODO missing check if there really is an inport and ep
+	eventProcessingStrategy->unsubscribe(s);
+}
+
+void Router::setQualityOfServiceSpecs(const QualityOfServiceSpec &specs) {
+	// TODO only required if we also want to support QoS for ingoing ports
+	// aswell
+	//	if (inConnector) {
+	//		inConnector->setQualityOfServiceSpecs(specs);
+	//	}
+	if (outConnector) {
+		outConnector->setQualityOfServiceSpecs(specs);
+	}
+}
+
+ConnectorPtr Router::getOutConnector() {
+	return outConnector;
+}
+
+ConnectorPtr Router::getInConnector() {
+	return inConnector;
+}
+
+}
+}
+
---
+++ src/rsb/eventprocessing/Router.h
@@ -0,0 +1,119 @@
+/* ============================================================
+ *
+ * This file is a part of the RSB project
+ *
+ * Copyright (C) 2010 by Sebastian Wrede <swrede at techfak dot uni-bielefeld dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#pragma once
+
+#include <boost/shared_ptr.hpp>
+
+#include <rsc/logging/Logger.h>
+
+#include "../transport/Factory.h"
+#include "../filter/FilterObservable.h"
+#include "EventProcessingStrategy.h"
+#include "../Subscription.h"
+#include "../QualityOfServiceSpec.h"
+#include "rsb/rsbexports.h"
+
+namespace rsb {
+namespace eventprocessing {
+
+/**
+ *
+ *
+ * @author swrede
+ * @todo extend incoming and outgoing ports to sets of ports
+ * @todo add configuration, provide preliminary set up interface
+ * @todo implement abstract factory pattern for different port types
+ * @todo think about null objects for ports to avoid checks for existence
+ */
+class RSB_EXPORT Router { //: //public rsb::filter::FilterObservable {
+public:
+	Router(
+			transport::Factory::ConnectorTypes inType =
+					transport::Factory::LOCAL,
+			transport::Factory::ConnectorTypes outType =
+					transport::Factory::LOCAL);
+	virtual ~Router();
+
+	void activate();
+	void deactivate();
+
+	/**
+	 * Publish event to out ports of this router.
+	 *
+	 * @param e event to publish
+	 */
+	void publish(EventPtr e);
+
+	/**
+	 * Add a subscription.
+	 *
+	 * @param s subscription to add
+	 */
+	void subscribe(rsb::SubscriptionPtr s);
+
+	/**
+	 * Unsubscribe a subscription.
+	 *
+	 * @param s subscription to remove
+	 */
+	void unsubscribe(rsb::SubscriptionPtr s);
+
+	/**
+	 * Define the desired quality of service specifications for published
+	 * events.
+	 *
+	 * @param specs QoS specification
+	 * @throw UnsupportedQualityOfServiceException requirements cannot be met
+	 */
+	void setQualityOfServiceSpecs(const QualityOfServiceSpec &specs);
+
+	/**
+	 * @name unit testing interface
+	 *
+	 * HACK for current tests.
+	 */
+	//@{
+	transport::ConnectorPtr getOutConnector();
+	transport::ConnectorPtr getInConnector();
+	//@}
+
+protected:
+	/**
+	 * Helper for port notification about subscription status changes.
+	 */
+	void
+			notifyConnectors(rsb::SubscriptionPtr s,
+					rsb::filter::FilterAction::Types a);
+
+private:
+	rsc::logging::LoggerPtr logger;
+	transport::ConnectorPtr inConnector;
+	transport::ConnectorPtr outConnector;
+	// ep for observation model
+	EventProcessingStrategyPtr eventProcessingStrategy;
+	volatile bool shutdown;
+
+};
+
+typedef boost::shared_ptr<Router> RouterPtr;
+
+}
+}
+
