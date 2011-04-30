eee164473a14fb60360287fe94bc9154e64a93cb
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-04-30 13:19:58 +0000

    unnecessary second cancel and join for the informer

--- test/rsb/transport/spread/SpreadConnectorTest.cpp
+++ test/rsb/transport/spread/SpreadConnectorTest.cpp
@@ -106,6 +106,4 @@ TEST(SpreadConnectorTest, testRoundtrip)
     // port is deactivated through dtr
     cerr << "SpreadProcessTest finished" << endl;
 
-    source->cancel();
-    source->waitDone();
 }
