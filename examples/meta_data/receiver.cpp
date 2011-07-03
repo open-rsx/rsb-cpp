df6d0be061e32931067868aa23f59b5140250906
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-07-03 13:36:59 +0000

    Added an example for meta-infos and user timestamps in examples/ refs #396 * examples/CMakeLists.txt: added files   examples/meta_data/receiver.cpp, examples/meta_data/sender.cpp * examples/meta_data/receiver.cpp: new file; contains the receiving   part of the meta-data example * examples/meta_data/sender.cpp: new file; contains the sending part   of the meta-data example

---
+++ examples/meta_data/receiver.cpp
@@ -0,0 +1,67 @@
+/* ============================================================
+ *
+ * This file is part of the RSB project
+ *
+ * Copyright (C) 2011 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include <rsb/Event.h>
+#include <rsb/Listener.h>
+#include <rsb/Factory.h>
+
+using namespace std;
+
+using namespace boost;
+
+using namespace rsb;
+
+// This function is called for each received event by the Listener
+// object created below.
+void printEvent(EventPtr event) {
+    cout << "Received event with meta-data:" << endl;
+
+    // Read-only access to the meta-data of the event.
+    MetaData metaData = event->getMetaData();
+    // Iterate over all user-supplied timestamps. There are accessor
+    // methods like getCreateTime() for the system-supplied
+    // timestamps.
+    for (map<string, uint64_t>::const_iterator it = metaData.userTimesBegin();
+         it != metaData.userTimesEnd(); ++it) {
+        cout << it->first << ": " << it->second << endl;
+    }
+    // Iterate over all meta-data items.
+    for (map<string, string>::const_iterator it = metaData.userInfosBegin();
+         it != metaData.userInfosEnd(); ++it) {
+        cout << it->first << ": " << it->second << endl;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Create a Listener object for the channel of this example and
+    // connect the printEvent() function using an EventFunctionHandler
+    // adapter.
+    ListenerPtr listener
+        = Factory::getInstance().createListener(Scope("/example/metadata"));
+    listener->addHandler(HandlerPtr(new EventFunctionHandler(printEvent)));
+
+    // Wait here, while the Listener object asynchronously receives
+    // and processes events.
+    while (true) {
+        sleep(1);
+    };
+
+    return EXIT_SUCCESS;
+}
---
+++ examples/meta_data/sender.cpp
@@ -0,0 +1,68 @@
+/* ============================================================
+ *
+ * This file is part of the RSB project.
+ *
+ * Copyright (C) 2011 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include <string>
+
+#include <rsc/misc/langutils.h>
+
+#include <rsb/Event.h>
+#include <rsb/Factory.h>
+
+using namespace std;
+
+using namespace rsb;
+
+int main() {
+    // Create an Informer object for string data for the channel of
+    // this example.
+    Informer<string>::Ptr informer
+        = Factory::getInstance().createInformer<string>(Scope("/example/metadata"));
+
+    // Create and send data in loop.
+    while (true) {
+        // We simulate some expensive data-creation process here, so
+        // we get distinct timestamps at the start and end of the
+        // processing.
+        boost::uint64_t processingStart = rsc::misc::currentTimeMicros();
+	sleep(1);
+	string data = "result of processing";
+        boost::uint64_t processingDone = rsc::misc::currentTimeMicros();
+
+        // We manually create a new Event object and add the desired
+        // meta-data:
+        // + processing-start timestamp
+        // + processing-done timestamp
+        // + creator meta-info item
+	EventPtr event(new Event(Scope("/example/metadata"),
+                                 Informer<string>::DataPtr(new string(data)),
+                                 "std::string"));
+
+        MetaData &metaData = event->mutableMetaData();
+        metaData.setUserTime("processing-start", processingStart);
+        metaData.setUserTime("processing-done",  processingDone);
+
+        metaData.setUserInfo("creator", "example-sender-program");
+
+        // When the data members of the Event object are populated, we
+        // publish it using the Informer object.
+        informer->publish(event);
+    }
+
+    return EXIT_SUCCESS;
+}
