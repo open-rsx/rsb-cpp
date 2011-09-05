50a0bc31530064d4daf6584753cade3a38f2c509
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-09-05 16:09:11 +0000

    Added src/rsb/eventprocessing/DirectEventReceivingStrategy.{h,cpp} * src/CMakeLists.txt: added files   src/rsb/eventprocessing/DirectEventReceivingStrategy.{h,cpp} * src/rsb/eventprocessing/DirectEventReceivingStrategy.{h,cpp}: new   files; interface and implementation of DirectEventReceivingStrategy

---
+++ src/rsb/eventprocessing/DirectEventReceivingStrategy.cpp
@@ -0,0 +1,184 @@
+/* ============================================================
+ *
+ * This file is part of the RSB project
+ *
+ * Copyright (C) 2011 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "DirectEventReceivingStrategy.h"
+
+#include <rsc/debug/DebugTools.h>
+#include <rsc/runtime/ContainerIO.h>
+#include <rsc/misc/langutils.h>
+
+using namespace std;
+
+using namespace rsc::logging;
+
+namespace rsb {
+namespace eventprocessing {
+
+DirectEventReceivingStrategy::DirectEventReceivingStrategy() :
+    logger(Logger::getLogger("rsb.eventprocessing.DirectEventReceivingStrategy")),
+    errorStrategy(ParticipantConfig::LOG) {
+}
+
+DirectEventReceivingStrategy::~DirectEventReceivingStrategy() {
+}
+
+void DirectEventReceivingStrategy::printContents(ostream &stream) const {
+    boost::shared_lock<boost::shared_mutex> filtersLock(filtersMutex);
+    boost::recursive_mutex::scoped_lock errorLock(errorStrategyMutex);
+    stream << "filters = " << filters << ", errorStrategy = " << errorStrategy;
+}
+
+void DirectEventReceivingStrategy::setHandlerErrorStrategy(
+        const ParticipantConfig::ErrorStrategy &strategy) {
+    boost::recursive_mutex::scoped_lock lock(errorStrategyMutex);
+    this->errorStrategy = strategy;
+}
+
+bool DirectEventReceivingStrategy::filter(EventPtr e) {
+    // match event
+    try {
+
+        boost::shared_lock<boost::shared_mutex> lock(filtersMutex);
+        for (set<filter::FilterPtr>::const_iterator filterIt = filters.begin(); filterIt
+                != filters.end(); ++filterIt) {
+            if (!(*filterIt)->match(e)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    } catch (const exception& ex) {
+
+        stringstream s;
+        s << "Exception matching event " << e << ":" << endl;
+        s << ex.what() << endl;
+        s << rsc::debug::DebugTools::newInstance()->exceptionInfo(ex);
+
+        handleDispatchError(s.str());
+
+    } catch (...) {
+
+        stringstream s;
+        s << "Catch-all handler called matching event " << endl;
+        rsc::debug::DebugToolsPtr tool = rsc::debug::DebugTools::newInstance();
+        vector<string> trace = tool->createBacktrace();
+        s << tool->formatBacktrace(trace);
+
+        handleDispatchError(s.str());
+
+    }
+
+    return false;
+
+}
+
+void DirectEventReceivingStrategy::handleDispatchError(const string &message) {
+
+    boost::recursive_mutex::scoped_lock strategyLock(errorStrategyMutex);
+    switch (errorStrategy) {
+    case ParticipantConfig::LOG:
+        RSCERROR(logger, message)
+        ;
+        break;
+    case ParticipantConfig::PRINT:
+        cerr << message << endl;
+        break;
+    case ParticipantConfig::EXIT:
+        cerr << message << endl;
+        exit(1);
+        break;
+    default:
+        RSCWARN(logger, "Unknown error strategy: " << errorStrategy)
+        ;
+        RSCERROR(logger, message)
+        ;
+        break;
+    }
+
+}
+
+void DirectEventReceivingStrategy::deliver(rsb::HandlerPtr handler, EventPtr e) {
+    RSCDEBUG(logger, "Delivering event " << e << " to handler " << handler);
+
+    try {
+
+        handler->handle(e);
+
+    } catch (const exception& ex) {
+
+        stringstream s;
+        s << "Exception dispatching event " << e << " to handler " << handler
+                << ":" << endl;
+        s << ex.what() << endl;
+        s << rsc::debug::DebugTools::newInstance()->exceptionInfo(ex);
+
+        handleDispatchError(s.str());
+
+    } catch (...) {
+
+        stringstream s;
+        s << "Catch-all handler called dispatching event " << e
+                << " to handler " << handler << endl;
+        rsc::debug::DebugToolsPtr tool = rsc::debug::DebugTools::newInstance();
+        vector<string> trace = tool->createBacktrace();
+        s << tool->formatBacktrace(trace);
+
+        handleDispatchError(s.str());
+
+    }
+
+}
+
+void DirectEventReceivingStrategy::handle(EventPtr event) {
+    boost::shared_lock<boost::shared_mutex> lock(this->handlerMutex);
+
+    if (filter(event)) {
+        for (HandlerList::const_iterator it = this->handlers.begin();
+             it != this->handlers.end(); ++it)
+            deliver(*it, event);
+    }
+}
+
+void DirectEventReceivingStrategy::addHandler(rsb::HandlerPtr handler,
+        const bool &/*wait*/) {
+    boost::shared_lock<boost::shared_mutex> lock(this->handlerMutex);
+
+    this->handlers.push_back(handler);
+}
+
+void DirectEventReceivingStrategy::removeHandler(rsb::HandlerPtr handler,
+        const bool &/*wait*/) {
+    boost::shared_lock<boost::shared_mutex> lock(this->handlerMutex);
+
+    this->handlers.remove(handler);
+}
+
+void DirectEventReceivingStrategy::addFilter(filter::FilterPtr filter) {
+    boost::unique_lock<boost::shared_mutex> lock(filtersMutex);
+    filters.insert(filter);
+}
+
+void DirectEventReceivingStrategy::removeFilter(filter::FilterPtr filter) {
+    boost::unique_lock<boost::shared_mutex> lock(filtersMutex);
+    filters.erase(filter);
+}
+
+}
+}
---
+++ src/rsb/eventprocessing/DirectEventReceivingStrategy.h
@@ -0,0 +1,89 @@
+/* ============================================================
+ *
+ * This file is part of the RSB project
+ *
+ * Copyright (C) 2011 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#pragma once
+
+#include <list>
+#include <set>
+#include <utility>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/thread/shared_mutex.hpp>
+
+#include <rsc/logging/Logger.h>
+#include <rsc/threading/OrderedQueueDispatcherPool.h>
+
+#include "../Event.h"
+#include "PushEventReceivingStrategy.h"
+#include "rsb/rsbexports.h"
+
+namespace rsb {
+namespace eventprocessing {
+
+/**
+ * This push-style event receiving strategy uses one or more threads
+ * to filter @ref rsb::Event s and dispatch matching events to @ref
+ * rsb::Handler s.
+ *
+ * @author jmoringe
+ */
+class RSB_EXPORT DirectEventReceivingStrategy: public PushEventReceivingStrategy {
+public:
+    DirectEventReceivingStrategy();
+
+    virtual ~DirectEventReceivingStrategy();
+
+    void printContents(std::ostream &stream) const;
+
+    void setHandlerErrorStrategy(const ParticipantConfig::ErrorStrategy &strategy);
+
+    // Qualification of HandlerPtr is required since there is another
+    // HandlerPtr type in eventprocessing.
+    virtual void addHandler(rsb::HandlerPtr handler, const bool &wait);
+    virtual void removeHandler(rsb::HandlerPtr handler, const bool &wait);
+
+    virtual void addFilter(filter::FilterPtr filter);
+    virtual void removeFilter(filter::FilterPtr filter);
+
+    void handle(EventPtr e);
+
+private:
+    typedef std::list<rsb::HandlerPtr> HandlerList;
+
+    // Qualification of HandlerPtr is required since there is another
+    // HandlerPtr type in eventprocessing.
+    bool filter(EventPtr event);
+    void deliver(rsb::HandlerPtr handler, EventPtr event);
+
+    void handleDispatchError(const std::string &message);
+
+    rsc::logging::LoggerPtr logger;
+
+    mutable boost::shared_mutex filtersMutex;
+    std::set<filter::FilterPtr> filters;
+
+    mutable boost::recursive_mutex errorStrategyMutex;
+    ParticipantConfig::ErrorStrategy errorStrategy;
+
+    mutable boost::shared_mutex handlerMutex;
+    HandlerList handlers;
+};
+
+}
+}
