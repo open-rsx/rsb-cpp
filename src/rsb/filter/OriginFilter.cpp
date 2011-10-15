9f735dc4b29184b5e50415e5b94f9c3f99016745
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-10-15 16:53:03 +0000

    Added OriginFilter class in src/rsb/filter/OriginFilter.{h,cpp} fixes #633 * src/rsb/filter/OriginFilter.{h,cpp}: new files; interface and   implementation of OriginFilter class * src/CMakeLists.txt: added files src/rsb/filter/OriginFilter.{h,cpp}

---
+++ src/rsb/filter/OriginFilter.cpp
@@ -0,0 +1,52 @@
+/** ============================================================
+ *
+ * This file is part of the RSB project.
+ *
+ * Copyright (C) 2011 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * ============================================================  */
+
+#include "OriginFilter.h"
+
+#include "FilterObserver.h"
+
+using namespace rsc::misc;
+
+namespace rsb {
+namespace filter {
+
+OriginFilter::OriginFilter(const UUID &origin,
+                           bool        invert):
+    origin(origin), invert(invert) {
+}
+
+UUID OriginFilter::getOrigin() const {
+    return this->origin;
+}
+
+bool OriginFilter::isInverted() const {
+    return this->invert;
+}
+
+bool OriginFilter::match(EventPtr e) {
+    bool result = this->origin == e->getMetaData().getSenderId();
+    return this->invert ? !result : result;
+}
+
+void OriginFilter::notifyObserver(FilterObserverPtr   fo,
+                                  FilterAction::Types at) {
+    fo->notify(this, at);
+}
+
+}
+}
---
+++ src/rsb/filter/OriginFilter.h
@@ -0,0 +1,61 @@
+/** ============================================================
+ *
+ * This file is part of the RSB project.
+ *
+ * Copyright (C) 2011 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * ============================================================  */
+
+#pragma once
+
+#include <rsc/misc/UUID.h>
+
+#include "Filter.h"
+
+namespace rsb {
+namespace filter {
+
+/**
+ * This filter matches events that originate from a particular
+ * participant.
+ *
+ * @author jmoringe
+ */
+class OriginFilter: public Filter {
+public:
+    /**
+     * Creates a new origin filter that matches event originating from
+     * @a origin.
+     *
+     * @param origin Id of the participant from which matching events
+     *               have to originate.
+     * @param invert If true, events match if they do @b not originate
+     *               from @a origin.
+     */
+    OriginFilter(const rsc::misc::UUID &origin,
+                 bool                   invert = false);
+
+    rsc::misc::UUID getOrigin() const;
+
+    bool isInverted() const;
+
+    bool match(EventPtr e);
+
+    void notifyObserver(FilterObserverPtr fo, FilterAction::Types at);
+private:
+    rsc::misc::UUID origin;
+    bool            invert;
+};
+
+}
+}
