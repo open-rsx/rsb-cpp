c309a0ff859923e11dda39a949066c680489f495
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-08-30 19:37:59 +0200

    Renamed send and version programs; Added Debian alternatives for both fixes #1157 * apps/info/main.cpp: renamed apps/{rsbversion/rsbversion ->   info/main}.cpp * apps/send/main.cpp: renamed apps/{rsbsend/rsbsend -> send/main}.cpp * apps/CMakeLists.txt * CMakeLists.txt * cpack/CPackDebianConfig.cmake

--- cpack/CPackDebianConfig.cmake
+++ cpack/CPackDebianConfig.cmake
@@ -49,6 +49,28 @@ ELSE()
     SET(CPACK_DEBIAN_PACKAGE_CONFLICTS "${RSB_NAME}-spread${VERSION_SUFFIX}")
 ENDIF()
 
+# Generate postinst and prerm hooks
+SET(PACKAGE_ALT_PRIORITY "80")
+
+SET(POSTINST_SCRIPT      "${CMAKE_CURRENT_BINARY_DIR}/postinst")
+SET(PRERM_SCRIPT         "${CMAKE_CURRENT_BINARY_DIR}/prerm")
+FILE(WRITE "${POSTINST_SCRIPT}" "#!/bin/sh\n\nset -e\n")
+FILE(WRITE "${PRERM_SCRIPT}"    "#!/bin/sh\n\nset -e\n")
+FOREACH(TOOL ${TOOLS})
+    FILE(APPEND "${POSTINST_SCRIPT}"
+                "update-alternatives --install                      \\
+                   /usr/bin/${BINARY_PREFIX}${TOOL}                 \\
+                   ${BINARY_PREFIX}${TOOL}                          \\
+                   /usr/bin/${BINARY_PREFIX}${TOOL}${BINARY_SUFFIX} \\
+                   ${PACKAGE_ALT_PRIORITY}\n\n")
+    FILE(APPEND "${PRERM_SCRIPT}"
+                "update-alternatives --remove                           \\
+                   ${BINARY_PREFIX}${TOOL}                              \\
+                   /usr/bin/${BINARY_PREFIX}${TOOL}${BINARY_SUFFIX}\n\n")
+ENDFOREACH()
+EXECUTE_PROCESS(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${PRERM_SCRIPT}")
+SET(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${POSTINST_SCRIPT};${PRERM_SCRIPT}")
+
 SET(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}_${CMAKE_SYSTEM_PROCESSOR}")
 
 MESSAGE(STATUS "Debian Package: ${CPACK_DEBIAN_PACKAGE_NAME} (${CPACK_DEBIAN_PACKAGE_VERSION}) [${CPACK_PACKAGE_FILE_NAME}.deb]")
