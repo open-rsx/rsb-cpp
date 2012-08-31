56789060ad7dfb8595b8762ac800cb86befba985
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-08-31 04:19:11 +0200

    Fixed Debian compliance issues in cpack/CPackDebianConfig.cmake
    * CMakeLists.txt: do not install COPYING.txt since Debian does this
      differently and the usefulness is doubtful for other systems
    * cpack/CPackDebianConfig.cmake: call ldconfig from postinst and
      postrm scripts; generate shlibs control file; write and install
      Debian style copyright file which references system-wide license
      texts; write and install upstream and Debian changelog by
      transforming the git log

--- cpack/CPackDebianConfig.cmake
+++ cpack/CPackDebianConfig.cmake
@@ -1,3 +1,5 @@
+INCLUDE(CheckLSBTypes)
+
 IF(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
     MESSAGE(FATAL_ERROR "Cannot configure CPack to generate Debian packages on non-linux systems.")
 ENDIF()
@@ -5,19 +7,18 @@ ENDIF()
 # To create a proper Debian/Ubuntu package, the following CMake
 # options should be used:
 
-#SET(BUILD_TESTS OFF)
-#SET(BUILD_EXAMPLES OFF)
+# SET(BUILD_TESTS OFF)
+# SET(BUILD_EXAMPLES OFF)
 
-#SET(CMAKE_BUILD_TYPE Release)
+# SET(CMAKE_BUILD_TYPE Release)
 SET(CPACK_STRIP_FILES "TRUE")
 
 # Actual packaging options
 
 SET(CPACK_GENERATOR "DEB")
 
-SET(CPACK_PACKAGE_VERSION            "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}${CPACK_PACKAGE_REVISION}")
 SET(CPACK_DEBIAN_PACKAGE_NAME        "lib${RSB_NAME}${VERSION_SUFFIX}")
-SET(CPACK_DEBIAN_PACKAGE_VERSION     "${CPACK_PACKAGE_VERSION}")
+SET(CPACK_DEBIAN_PACKAGE_VERSION     "${CPACK_PACKAGE_VERSION}${CPACK_PACKAGE_REVISION}")
 SET(CPACK_DEBIAN_PACKAGE_MAINTAINER  "Sebastian Wrede <swrede@techfak.uni-bielefeld.de>")
 SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Robotics Service Bus (C++ Implementation)
  C++ library implementing the Robotics Service Bus (RSB), a
@@ -30,13 +31,35 @@ SET(CPACK_DEBIAN_PACKAGE_DEPENDS     "libc6, libprotoc-dev, libboost-regex-dev, 
 SET(CPACK_DEBIAN_PACKAGE_SUGGESTS    "doxygen, lcov, cppcheck, sloccount, protobuf-compiler")
 # build dependencies protobuf-compiler, rsbprotocol
 
-# Generate postinst and prerm hooks
+SET(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}_${CMAKE_SYSTEM_PROCESSOR}")
+
+# Additional control files
 SET(PACKAGE_ALT_PRIORITY "80")
 
-SET(POSTINST_SCRIPT      "${CMAKE_CURRENT_BINARY_DIR}/postinst")
-SET(PRERM_SCRIPT         "${CMAKE_CURRENT_BINARY_DIR}/prerm")
+SET(SHLIBS_FILE     "${CMAKE_CURRENT_BINARY_DIR}/shlibs")
+SET(POSTINST_SCRIPT "${CMAKE_CURRENT_BINARY_DIR}/postinst")
+SET(PRERM_SCRIPT    "${CMAKE_CURRENT_BINARY_DIR}/prerm")
+SET(POSTRM_SCRIPT   "${CMAKE_CURRENT_BINARY_DIR}/postrm")
+
+# Generate postinst, prerm and postrm hooks
 FILE(WRITE "${POSTINST_SCRIPT}" "#!/bin/sh\n\nset -e\n")
 FILE(WRITE "${PRERM_SCRIPT}"    "#!/bin/sh\n\nset -e\n")
+FILE(WRITE "${POSTRM_SCRIPT}"   "#!/bin/sh\n\nset -e\n")
+
+# Generate shlibs
+FILE(WRITE "${SHLIBS_FILE}"
+     "lib${RSB_NAME} ${SO_VERSION} ${CPACK_DEBIAN_PACKAGE_NAME}")
+FILE(APPEND "${POSTINST_SCRIPT}"
+     "if [ \"$1\" = \"configure\" ]; then
+        ldconfig
+fi
+")
+FILE(APPEND "${POSTRM_SCRIPT}"
+     "if [ \"$1\" = \"remove\" ]; then
+        ldconfig
+fi")
+
+# Update alternatives
 FOREACH(TOOL ${TOOLS})
     FILE(APPEND "${POSTINST_SCRIPT}"
                 "update-alternatives --install                      \\
@@ -49,9 +72,51 @@ FOREACH(TOOL ${TOOLS})
                    ${BINARY_PREFIX}${TOOL}                              \\
                    /usr/bin/${BINARY_PREFIX}${TOOL}${BINARY_SUFFIX}\n\n")
 ENDFOREACH()
-EXECUTE_PROCESS(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${PRERM_SCRIPT}")
-SET(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${POSTINST_SCRIPT};${PRERM_SCRIPT}")
 
-SET(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}_${CMAKE_SYSTEM_PROCESSOR}")
+EXECUTE_PROCESS(COMMAND chmod 644 "${SHLIBS_FILE}")
+EXECUTE_PROCESS(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${PRERM_SCRIPT}" "${POSTRM_SCRIPT}")
+SET(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${SHLIBS_FILE};${POSTINST_SCRIPT};${PRERM_SCRIPT};${POSTRM_SCRIPT}")
+
+# Write license file
+FILE(WRITE "${CMAKE_BINARY_DIR}/copyright"
+     "Copyright (C) 2010-2012 ${CPACK_DEBIAN_PACKAGE_MAINTAINER}
+
+   This software may be licensed under the terms of the
+   GNU Lesser General Public License Version 3 (the ``LGPL''),
+   or (at your option) any later version.
+
+   Software distributed under the License is distributed
+   on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+   express or implied. See the LGPL for the specific language
+   governing rights and limitations.
+
+   You should have received a copy of the LGPL along with this
+   program. If not, go to http://www.gnu.org/licenses/lgpl.html
+   or write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+On Debian systems, the complete text of the GNU Lesser General Public
+License can be found in `/usr/share/common-licenses/LGPL-3'.")
+INSTALL(FILES "${CMAKE_BINARY_DIR}/copyright"
+        DESTINATION "share/doc/${CPACK_DEBIAN_PACKAGE_NAME}")
+
+# Generate required change log files
+EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE}
+                        log "--format=%ad  %an  <%ae>%n%n%w(76,8,10)%s%w(76,8,8)%n%n%b%n"
+                        --date=short
+                COMMAND gzip -9
+                OUTPUT_FILE "${CMAKE_BINARY_DIR}/changelog.gz")
+EXECUTE_PROCESS(COMMAND sh -c "echo -n \"sed -e '\" ; for c in $(${GIT_EXECUTABLE} rev-list --all -- \"${CMAKE_CURRENT_LIST_FILE}\") ; do echo -n \"s/$c/$(${GIT_EXECUTABLE} describe --tags $c | sed -re s/[^0-9]*\\([0-9]+\\)\\.\\([0-9]+\\)-\\([0-9]+\\)-.*/\\\\1.\\'\\$\\(\\(\\\\2+1\\)\\)\\'.\\\\3/)/\\;\" ; done ; echo \"'\""
+                OUTPUT_VARIABLE RULES)
+EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE}
+                        log "--format=${CPACK_DEBIAN_PACKAGE_NAME} (%H) ${LSB_CODENAME}; urgency=low%n%n%w(76,8,10)%s%w(76,8,8)%n%n%b%n%n%w(200,1,1)-- %an <%ae>  %ad%n"
+                        --date=rfc
+                        -- "${CMAKE_CURRENT_LIST_FILE}"
+                COMMAND sh -c ${RULES}
+                COMMAND gzip -9
+                OUTPUT_FILE "${CMAKE_BINARY_DIR}/changelog.Debian.gz")
+INSTALL(FILES "${CMAKE_BINARY_DIR}/changelog.gz"
+              "${CMAKE_BINARY_DIR}/changelog.Debian.gz"
+        DESTINATION "share/doc/${CPACK_DEBIAN_PACKAGE_NAME}")
 
 MESSAGE(STATUS "Debian Package: ${CPACK_DEBIAN_PACKAGE_NAME} (${CPACK_DEBIAN_PACKAGE_VERSION}) [${CPACK_PACKAGE_FILE_NAME}.deb]")
