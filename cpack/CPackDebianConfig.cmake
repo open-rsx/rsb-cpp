6f14496537614398e43866bb2b1a2499fd6c441a
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2012-10-08 17:04:27 +0200

    For debian packaging do not separate between rsb with and without spread in the name.
    We can now alwazs assume that spread is available since it will be (in theory) deployed on our own server and spread is until 0.8 an essential part of RSB.

--- cpack/CPackDebianConfig.cmake
+++ cpack/CPackDebianConfig.cmake
@@ -17,43 +17,22 @@ SET(VERSION_SUFFIX "${RSB_VERSION_MAJOR}.${RSB_VERSION_MINOR}")
 
 INCLUDE(CheckLSBTypes)
 SET(CPACK_PACKAGE_VERSION   "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}${CPACK_PACKAGE_REVISION}")
-IF(BUILD_SPREAD_TRANSPORT)
-    SET(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}-spread${VERSION_SUFFIX}-${CPACK_PACKAGE_VERSION}_${LSB_PROCESSOR_ARCH}")
-ELSE()
-    SET(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}${VERSION_SUFFIX}-${CPACK_PACKAGE_VERSION}_${LSB_PROCESSOR_ARCH}")
-ENDIF()
+SET(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}${VERSION_SUFFIX}-${CPACK_PACKAGE_VERSION}_${LSB_PROCESSOR_ARCH}")
 
 SET(CPACK_GENERATOR "DEB")
 
 SET(CPACK_DEBIAN_PACKAGE_VERSION ${CPACK_PACKAGE_VERSION})
 SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "Sebastian Wrede <swrede@techfak.uni-bielefeld.de>")
-IF(WITH_SPREAD_TRANSPORT)
-    SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Robotics Service Bus (C++ Implementatation with builtin Spread transport)
- C++ library implementing the Robotics Service Bus (RSB), a
- lightweight, extensible, event-driven middleware for robotic system
- and other domain.
- .
- This package is built with support for a network transport based on
- the Spread group communication framework.")
-ELSE()
-    SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Robotics Service Bus (C++ Implementatation)
+SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Robotics Service Bus (C++ Implementatation)
  C++ library implementing the Robotics Service Bus (RSB), a
  lightweight, extensible, event-driven middleware for robotic system
  and other domain.")
-ENDIF()
 SET(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
 SET(CPACK_DEBIAN_PACKAGE_SECTION "devel")
 SET(CPACK_DEBIAN_ARCHITECTURE ${CMAKE_SYSTEM_PROCESSOR})
-SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6, libprotoc-dev, rsc${VERSION_SUFFIX}, libboost-regex-dev")
+SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6, libprotoc-dev, rsc${VERSION_SUFFIX}, libboost-regex-dev, spread (>= 4.0)")
 # build dependencies protobuf-compiler, rsbprotocol
 SET(CPACK_DEBIAN_PACKAGE_SUGGESTS "doxygen, lcov, cppcheck, sloccount, protobuf-compiler")
-IF(WITH_SPREAD_TRANSPORT)
-    SET(CPACK_DEBIAN_PACKAGE_NAME "rsb-spread${VERSION_SUFFIX}")
-    SET(CPACK_DEBIAN_PACKAGE_SUGGESTS "${CPACK_DEBIAN_PACKAGE_SUGGESTS}, spread (>= 4.0)")
-    # TODO(jmoringe): CPack does not seem to support conflicts
-    SET(CPACK_DEBIAN_PACKAGE_CONFLICTS "rsb${VERSION_SUFFIX}")
-ELSE()
-    SET(CPACK_DEBIAN_PACKAGE_NAME "rsb${VERSION_SUFFIX}")
-    # TODO(jmoringe): CPack does not seem to support conflicts
-    SET(CPACK_DEBIAN_PACKAGE_CONFLICTS "rsb-spread${VERSION_SUFFIX}")
-ENDIF()
+SET(CPACK_DEBIAN_PACKAGE_NAME "rsb${VERSION_SUFFIX}")
+# TODO(jmoringe): CPack does not seem to support conflicts
+SET(CPACK_DEBIAN_PACKAGE_CONFLICTS "rsb-spread${VERSION_SUFFIX}")
