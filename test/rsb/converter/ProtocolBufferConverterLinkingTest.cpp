cac97a942106cb72aba67fe996a83836b8941a64
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2012-12-10 14:48:21 +0100

    Backport: Prevent linking errors on windows with the ProtocolBufferConverter.
    Due to extending Convert<string> multiple conflicting template instatitation from the Converter base class could occur if the ProtocolBufferConverter was used across multiple compilation units.
    
    * Added a test case to reproduce the behavior
    * Added dll import statements to prevent the bug
    
    fixes #1285

---
+++ test/rsb/converter/ProtocolBufferConverterLinkingTest.cpp
@@ -0,0 +1,47 @@
+/* ============================================================
+ *
+ * This file is a part of the RSB project
+ *
+ * Copyright (C) 2012 by Johannes Wienke <jwienke at techfak dot uni-bielefeld dot de>
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "rsb/converter/ProtocolBufferConverter.h"
+#include "rsb/converter/TestMessage.pb.h"
+
+using namespace std;
+using namespace rsb;
+using namespace rsb::converter;
+using namespace testing;
+
+// This test tests linking errors with subclassing from a template-based class.
+// It is only effective if there is a second file doing exactly the same thing.
+// That's the reason why this file is duplicated in the source tree with
+// slightly different names.
+// For an explanation of the potential linking problems please refer to:
+// http://www.codesynthesis.com/~boris/blog/2010/01/18/dll-export-cxx-templates/
+TEST(ProtocolBufferConverterLinkingTest, testWindowsLinking)
+{
+    ProtocolBufferConverter<rsb::converter::TestMessage> converter;
+}
