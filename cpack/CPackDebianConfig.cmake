2bce98183e0c36a655f0196de2b16f13df78b8a9
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-04-26 20:30:34 +0200

    "Unscreamified" **/CMakeLists.txt
    * CMakeLists.txt: "unscreamified"
    * src/CMakeLists.txt: likewise
    * src/rsb/introspection/CMakeLists.txt: likewise
    * apps/CMakeLists.txt: likewise
    * test/CMakeLists.txt: likewise
    * examples/CMakeLists.txt: likewise
    * cpack/CPackDebianConfig.cmake: likewise

--- cpack/CPackDebianConfig.cmake
+++ cpack/CPackDebianConfig.cmake
@@ -1,8 +1,8 @@
-INCLUDE(CheckLSBTypes)
+include(CheckLSBTypes)
 
-IF(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    MESSAGE(FATAL_ERROR "Cannot configure CPack to generate Debian packages on non-linux systems.")
-ENDIF()
+if(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    message(FATAL_ERROR "Cannot configure CPack to generate Debian packages on non-linux systems.")
+endif()
 
 # To create a proper Debian/Ubuntu package, the following CMake
 # options should be used:
@@ -11,7 +11,7 @@ ENDIF()
 # SET(BUILD_EXAMPLES OFF)
 
 # SET(CMAKE_BUILD_TYPE Release)
-SET(CPACK_STRIP_FILES "TRUE")
+set(CPACK_STRIP_FILES "TRUE")
 
 #set(CPACK_INSTALL_PREFIX "/usr")
 set(CPACK_SET_DESTDIR On)
@@ -19,70 +19,70 @@ set(CMAKE_INSTALL_PREFIX ${CPACK_INSTALL_PREFIX})
 
 # Actual packaging options
 
-SET(CPACK_GENERATOR "DEB")
+set(CPACK_GENERATOR "DEB")
 
-SET(CPACK_DEBIAN_PACKAGE_NAME        "lib${RSB_NAME}${VERSION_SUFFIX}")
-SET(CPACK_DEBIAN_PACKAGE_VERSION     "${CPACK_PACKAGE_VERSION}${CPACK_PACKAGE_REVISION}")
-SET(CPACK_DEBIAN_PACKAGE_MAINTAINER  "Sebastian Wrede <swrede@techfak.uni-bielefeld.de>")
-SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Robotics Service Bus (C++ Implementation)
+set(CPACK_DEBIAN_PACKAGE_NAME        "lib${RSB_NAME}${VERSION_SUFFIX}")
+set(CPACK_DEBIAN_PACKAGE_VERSION     "${CPACK_PACKAGE_VERSION}${CPACK_PACKAGE_REVISION}")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER  "Sebastian Wrede <swrede@techfak.uni-bielefeld.de>")
+set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Robotics Service Bus (C++ Implementation)
  C++ library implementing the Robotics Service Bus (RSB), a
  lightweight, extensible, event-driven middleware for robotic system
  and other domain.")
-SET(CPACK_DEBIAN_PACKAGE_PRIORITY    "optional")
-SET(CPACK_DEBIAN_PACKAGE_SECTION     "devel")
+set(CPACK_DEBIAN_PACKAGE_PRIORITY    "optional")
+set(CPACK_DEBIAN_PACKAGE_SECTION     "devel")
 #SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE  "${CMAKE_SYSTEM_PROCESSOR}") # Debian uses different names here
-SET(CPACK_DEBIAN_PACKAGE_DEPENDS     "libc6, libprotoc-dev, libboost-regex-dev, librsc${VERSION_SUFFIX}")
-SET(CPACK_DEBIAN_PACKAGE_SUGGESTS    "doxygen, lcov, cppcheck, sloccount, protobuf-compiler")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS     "libc6, libprotoc-dev, libboost-regex-dev, librsc${VERSION_SUFFIX}")
+set(CPACK_DEBIAN_PACKAGE_SUGGESTS    "doxygen, lcov, cppcheck, sloccount, protobuf-compiler")
 # build dependencies protobuf-compiler, rsbprotocol
 
-SET(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}_${CMAKE_SYSTEM_PROCESSOR}")
+set(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}_${CMAKE_SYSTEM_PROCESSOR}")
 
 # Additional control files
-SET(PACKAGE_ALT_PRIORITY "80")
+set(PACKAGE_ALT_PRIORITY "80")
 
-SET(SHLIBS_FILE     "${CMAKE_CURRENT_BINARY_DIR}/shlibs")
-SET(POSTINST_SCRIPT "${CMAKE_CURRENT_BINARY_DIR}/postinst")
-SET(PRERM_SCRIPT    "${CMAKE_CURRENT_BINARY_DIR}/prerm")
-SET(POSTRM_SCRIPT   "${CMAKE_CURRENT_BINARY_DIR}/postrm")
+set(SHLIBS_FILE     "${CMAKE_CURRENT_BINARY_DIR}/shlibs")
+set(POSTINST_SCRIPT "${CMAKE_CURRENT_BINARY_DIR}/postinst")
+set(PRERM_SCRIPT    "${CMAKE_CURRENT_BINARY_DIR}/prerm")
+set(POSTRM_SCRIPT   "${CMAKE_CURRENT_BINARY_DIR}/postrm")
 
 # Generate postinst, prerm and postrm hooks
-FILE(WRITE "${POSTINST_SCRIPT}" "#!/bin/sh\n\nset -e\n")
-FILE(WRITE "${PRERM_SCRIPT}"    "#!/bin/sh\n\nset -e\n")
-FILE(WRITE "${POSTRM_SCRIPT}"   "#!/bin/sh\n\nset -e\n")
+file(WRITE "${POSTINST_SCRIPT}" "#!/bin/sh\n\nset -e\n")
+file(WRITE "${PRERM_SCRIPT}"    "#!/bin/sh\n\nset -e\n")
+file(WRITE "${POSTRM_SCRIPT}"   "#!/bin/sh\n\nset -e\n")
 
 # Generate shlibs
-FILE(WRITE "${SHLIBS_FILE}"
+file(WRITE "${SHLIBS_FILE}"
      "lib${RSB_NAME} ${SO_VERSION} ${CPACK_DEBIAN_PACKAGE_NAME}")
-FILE(APPEND "${POSTINST_SCRIPT}"
+file(APPEND "${POSTINST_SCRIPT}"
      "if [ \"$1\" = \"configure\" ]; then
         ldconfig
 fi
 ")
-FILE(APPEND "${POSTRM_SCRIPT}"
+file(APPEND "${POSTRM_SCRIPT}"
      "if [ \"$1\" = \"remove\" ]; then
         ldconfig
 fi")
 
 # Update alternatives
-FOREACH(TOOL ${TOOLS})
-    FILE(APPEND "${POSTINST_SCRIPT}"
+foreach(TOOL ${TOOLS})
+    file(APPEND "${POSTINST_SCRIPT}"
                 "update-alternatives --install                      \\
                    /usr/bin/${BINARY_PREFIX}${TOOL}                 \\
                    ${BINARY_PREFIX}${TOOL}                          \\
                    /usr/bin/${BINARY_PREFIX}${TOOL}${BINARY_SUFFIX} \\
                    ${PACKAGE_ALT_PRIORITY}\n\n")
-    FILE(APPEND "${PRERM_SCRIPT}"
+    file(APPEND "${PRERM_SCRIPT}"
                 "update-alternatives --remove                           \\
                    ${BINARY_PREFIX}${TOOL}                              \\
                    /usr/bin/${BINARY_PREFIX}${TOOL}${BINARY_SUFFIX}\n\n")
-ENDFOREACH()
+endforeach()
 
-EXECUTE_PROCESS(COMMAND chmod 644 "${SHLIBS_FILE}")
-EXECUTE_PROCESS(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${PRERM_SCRIPT}" "${POSTRM_SCRIPT}")
-SET(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${SHLIBS_FILE};${POSTINST_SCRIPT};${PRERM_SCRIPT};${POSTRM_SCRIPT}")
+execute_process(COMMAND chmod 644 "${SHLIBS_FILE}")
+execute_process(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${PRERM_SCRIPT}" "${POSTRM_SCRIPT}")
+set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${SHLIBS_FILE};${POSTINST_SCRIPT};${PRERM_SCRIPT};${POSTRM_SCRIPT}")
 
 # Write license file
-FILE(WRITE "${CMAKE_BINARY_DIR}/copyright"
+file(WRITE "${CMAKE_BINARY_DIR}/copyright"
      "Copyright (C) 2010-2012 ${CPACK_DEBIAN_PACKAGE_MAINTAINER}
 
    This software may be licensed under the terms of the
@@ -101,26 +101,26 @@ FILE(WRITE "${CMAKE_BINARY_DIR}/copyright"
 
 On Debian systems, the complete text of the GNU Lesser General Public
 License can be found in `/usr/share/common-licenses/LGPL-3'.")
-INSTALL(FILES "${CMAKE_BINARY_DIR}/copyright"
+install(FILES "${CMAKE_BINARY_DIR}/copyright"
         DESTINATION "share/doc/${CPACK_DEBIAN_PACKAGE_NAME}")
 
 # Generate required change log files
-EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE}
+execute_process(COMMAND ${GIT_EXECUTABLE}
                         log "--format=%ad  %an  <%ae>%n%n%w(76,8,10)%s%w(76,8,8)%n%n%b%n"
                         --date=short
                 COMMAND gzip -9
                 OUTPUT_FILE "${CMAKE_BINARY_DIR}/changelog.gz")
-EXECUTE_PROCESS(COMMAND sh -c "echo -n \"sed -e '\" ; for c in $(${GIT_EXECUTABLE} rev-list --all -- \"${CMAKE_CURRENT_LIST_FILE}\") ; do echo -n \"s/$c/$(${GIT_EXECUTABLE} describe --tags $c | sed -re s/[^0-9]*\\([0-9]+\\)\\.\\([0-9]+\\)-\\([0-9]+\\)-.*/\\\\1.\\'\\$\\(\\(\\\\2+1\\)\\)\\'.\\\\3/)/\\;\" ; done ; echo \"'\""
+execute_process(COMMAND sh -c "echo -n \"sed -e '\" ; for c in $(${GIT_EXECUTABLE} rev-list --all -- \"${CMAKE_CURRENT_LIST_FILE}\") ; do echo -n \"s/$c/$(${GIT_EXECUTABLE} describe --tags $c | sed -re s/[^0-9]*\\([0-9]+\\)\\.\\([0-9]+\\)-\\([0-9]+\\)-.*/\\\\1.\\'\\$\\(\\(\\\\2+1\\)\\)\\'.\\\\3/)/\\;\" ; done ; echo \"'\""
                 OUTPUT_VARIABLE RULES)
-EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE}
+execute_process(COMMAND ${GIT_EXECUTABLE}
                         log "--format=${CPACK_DEBIAN_PACKAGE_NAME} (%H) ${LSB_CODENAME}; urgency=low%n%n%w(76,8,10)%s%w(76,8,8)%n%n%b%n%n%w(200,1,1)-- %an <%ae>  %ad%n"
                         --date=rfc
                         -- "${CMAKE_CURRENT_LIST_FILE}"
                 COMMAND sh -c ${RULES}
                 COMMAND gzip -9
                 OUTPUT_FILE "${CMAKE_BINARY_DIR}/changelog.Debian.gz")
-INSTALL(FILES "${CMAKE_BINARY_DIR}/changelog.gz"
+install(FILES "${CMAKE_BINARY_DIR}/changelog.gz"
               "${CMAKE_BINARY_DIR}/changelog.Debian.gz"
         DESTINATION "share/doc/${CPACK_DEBIAN_PACKAGE_NAME}")
 
-MESSAGE(STATUS "Debian Package: ${CPACK_DEBIAN_PACKAGE_NAME} (${CPACK_DEBIAN_PACKAGE_VERSION}) [${CPACK_PACKAGE_FILE_NAME}.deb]")
+message(STATUS "Debian Package: ${CPACK_DEBIAN_PACKAGE_NAME} (${CPACK_DEBIAN_PACKAGE_VERSION}) [${CPACK_PACKAGE_FILE_NAME}.deb]")
